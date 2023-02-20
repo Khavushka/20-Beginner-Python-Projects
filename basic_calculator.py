13cd9e063e5910335f0649fa19c36b09ee4ae2f7
Khavushka <khavushka@gmail.com>
2023-02-20 21:35:44 +0100

    if statement check operation and than print answer

--- basic_calculator.py
+++ basic_calculator.py
@@ -3,19 +3,43 @@
 # ask for values
 # call the functions
 # while loop to continue the program until the user wants to exit
+print("A. addition \nB. Subtraction \nC. Multiplication \nD. Division")
+
+c = input("Input your choise: ")
 
 def add(a,b):
    answer = a + b
-   print(int(a) + " + " + int(b) + " = " + str(answer))
+   print(str(a) + " + " + str(b) + " = " + str(answer))
 
 def sub(a,b):
     answer = a - b
-    print(int(a) + " - " + int(b) + " = " + str(answer))
+    print(str(a) + " - " + str(b) + " = " + str(answer))
 
 def mul(a,b):
     answer = a * b
-    print(int(a) + " * " + int(b) + " = " + str(answer))
+    print(str(a) + " * " + str(b) + " = " + str(answer))
 
 def div(a,b):
     answer = a / b
-    print(int(a) + " / " + int(b) + " = " + str(answer))
+    print(str(a) + " / " + str(b) + " = " + str(answer))
+
+if c == "a" or c == "A":
+    print("Addition")
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    add(a,b)
+elif c == "b" or c== "B":
+    print("Substraction")
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    sub(a,b)
+elif c == "c" or c== "C":
+    print("Multiplication")
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    mul(a,b)
+elif c == "d" or c== "D":
+    print("Division")
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    div(a,b)
