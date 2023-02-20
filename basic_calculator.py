ddd2f336121e3c2d7796dfdb1043db60d07be33c
Khavushka <khavushka@gmail.com>
2023-02-20 20:55:33 +0100

    four different functons

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,3 +1,17 @@
-# define the function needed: add, sub, nul, div
+# define the function needed: add, sub, mul, div
 # print options to the user
+# ask for values
+# call the functions
+# while loop to continue the program until the user wants to exit
 
+def add(a,b):
+   return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    return a / b
