ddc67a63d347508cf7e6d2aa10c5c57a569dc3f7
Khavushka <khavushka@gmail.com>
2023-02-21 14:12:26 +0100

    Collect the email

--- email_slicer.py
+++ email_slicer.py
@@ -1,3 +1,11 @@
 # collect email from user
 # slice and split the email using the @
 # the first part as the user name
+# the second part is gonna be saved as domain
+# split domain using . , 
+
+def main():
+    print("Welcome to e-mail slicer ")
+    print("")
+
+    
