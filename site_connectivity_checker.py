c64f6a120e7178c115b9d42275348a67f19aff39
Khavushka <khavushka@gmail.com>
2023-02-26 22:15:11 +0100

    Site connectivity checker

---
+++ site_connectivity_checker.py
@@ -0,0 +1,10 @@
+# import urllib
+# use urllib.request to get the data from the url
+# write a function that takes a url
+# returns a response
+
+import urllib.request as urllib
+
+def main():
+    print("This is a site conectivity checker program")
+    print("Input the url of the site")
