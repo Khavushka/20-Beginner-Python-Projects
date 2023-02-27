5c6e22d410d460cf069f226ac424bae5ff9d2fbb
Khavushka <khavushka@gmail.com>
2023-02-27 08:46:40 +0100

    importing packages and function to url, that prints

--- site_connectivity_checker.py
+++ site_connectivity_checker.py
@@ -5,6 +5,12 @@
 
 import urllib.request as urllib
 
-def main():
-    print("This is a site conectivity checker program")
-    print("Input the url of the site")
+print("This is a site conectivity checker program")
+input_url = input("Input the url of the site you want to check")
+
+def main(url):
+    print("Checking connectivity ")
+    response = urllib.urlopen(url)
+
+    print("Connected to", url, "succesfully")
+    print("The response code was", response.getcode())
