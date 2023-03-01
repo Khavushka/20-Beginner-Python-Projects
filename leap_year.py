981ea079ee421bf4697af4452914c390154853a9
Khavushka <khavushka@gmail.com>
2023-03-01 12:43:36 +0100

    Finish with improving leap year

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,15 @@
 # Leap year checker using Python
 
-def is_leap_year():
+def is_leap_year(year):
     if year % 4 == 0:
-        pass
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print('Leap year')
+            else: 
+                print('Not leap year')
+        else: 
+            print('Leap year')
     else:
         print("Not leap year")
+
+is_leap_year(2024)
