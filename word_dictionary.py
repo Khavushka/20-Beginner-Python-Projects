7abd1df2660c11a0c3b7387c9572a8a2f735cd7b
Khavushka <khavushka@gmail.com>
2023-03-01 12:50:51 +0100

    new project word dictionary

---
+++ word_dictionary.py
@@ -0,0 +1,21 @@
+# word dictionary using Python
+# have a python dictionary 
+#that has a key/value pair that represents a word and it's definition
+# collect input from the user, input is a word
+# print the definition
+
+def main():
+    word_dictionary = {
+        'hi': 'A way of greeting',
+        'eyes': 'an organ for seeing',
+        'earth': 'a planet in space'
+    }
+
+    while True:
+        word = input("Enter a word: ")
+        if word == "":
+            break
+        if word in word_dictionary:
+            print(word, ":", word_dictionary)
+
+main()
