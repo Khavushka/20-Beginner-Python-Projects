f6311a79ee9c21ef492e714aa9f65998583e577a
Khavushka <khavushka@gmail.com>
2023-03-01 07:22:28 +0100

    Currency converter from dollars to pounds using Python

--- currency_converter.py
+++ currency_converter.py
@@ -1,1 +1,14 @@
-#
+# currency converter using Python
+
+def main():
+    print("Thin program converts US dollars to Pounds Sterling")
+    print()
+
+    dollars = eval(input("Enter amount in dollars: "))
+
+    pounds = convert_to_pounds(dollars)
+    print("This is", pounds, "pounds")
+
+convert_to_pounds = lambda dollars: dollars * 0.82
+
+main()
