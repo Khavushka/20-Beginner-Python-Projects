ce0bcacd5d417620088f89e004ffde9ccae21e2f
Khavushka <khavushka@gmail.com>
2023-03-02 17:24:23 +0100

    Install cv2 package

--- face_detection.py
+++ face_detection.py
@@ -1,2 +1,12 @@
 # Face detection using Python
 # Install opencv-python package
+# Du skal bruge et billede til at blive genkendt
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Load image 
+image = cv2.imread('image.jpg')
+
+#Convert the image to grayscale
+grayscale = cv2.cvColor(image, cv2.COLOR_BGR2GRAY)
