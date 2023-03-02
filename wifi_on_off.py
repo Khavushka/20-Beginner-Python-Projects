d5fa6a9a316e4f9bc3a68ef3b90f45bf52e5282a
Khavushka <khavushka@gmail.com>
2023-03-02 17:31:07 +0100

    App to turn on and off WiFi

---
+++ wifi_on_off.py
@@ -0,0 +1,13 @@
+# Her vil jeg gerne udvikle en app til at slukke og starte wifi
+# Get the interface name
+# Inport package
+
+import os
+os.system("netsh interface show interface" )
+
+# Then replace it with wifi in this script to get going
+def enable():
+    os.system("netsh interface set interface 'Wifi' enabled")
+
+def disable():
+    os.system("netsh interface set interface 'Wifi' disabled")
