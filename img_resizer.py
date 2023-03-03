53cf451b0232d7787db57cb5da32eea4315c16cb
Khavushka <khavushka@gmail.com>
2023-03-03 21:24:32 +0100

    New project. Image resizer using python

---
+++ img_resizer.py
@@ -0,0 +1,7 @@
+# Image resizer using Python
+# install pillow if you havent
+# import pillow
+# open up the image we want to resize using python
+# specify the size we wanna change it to
+# save the new resized image
+
