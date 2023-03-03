1538eaf97e35296133d59d9a6d6b24adf1613bb5
Khavushka <khavushka@gmail.com>
2023-03-03 21:45:42 +0100

    Error with file directory

--- img_resizer.py
+++ img_resizer.py
@@ -5,3 +5,12 @@
 # specify the size we wanna change it to
 # save the new resized image
 
+from PIL import Image
+
+image = Image.open('\img\sunrise.jpg')
+
+print(f'Current size: {image.size}')
+
+resized_image = image.resize((500, 500))
+
+resized_image.save('new_image.jpg')
