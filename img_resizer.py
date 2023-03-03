e237589620b41f7de07c471aaff09918a9409f74
Khavushka <khavushka@gmail.com>
2023-03-03 21:48:58 +0100

    It's working now

--- img_resizer.py
+++ img_resizer.py
@@ -7,10 +7,10 @@
 
 from PIL import Image
 
-image = Image.open('\img\sunrise.jpg')
+image = Image.open('img/sunrise.jpg')
 
 print(f'Current size: {image.size}')
 
 resized_image = image.resize((500, 500))
 
-resized_image.save('new_image.jpg')
+resized_image.save('img/new_image.jpg')
