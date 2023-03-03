f58e7703c7b249ed10c4abfe0b1de972dad0d486
Khavushka <khavushka@gmail.com>
2023-03-03 21:19:37 +0100

    Remember to change mobilenumber

--- automation.py
+++ automation.py
@@ -4,12 +4,28 @@
 # https://textbelt.com/
 # pip install requests
 
+import schedule
+import time
 import requests
 
 def send_message():
     resp = requests.post('https://textbelt.com/text', {
-    'phone': '60833306',
-    'message': 'Hej, smukke',
+    'phone': 'mobilenumber her',
+    'message': 'Hej, smukke. I love you!!! Mom',
     'key': 'textbelt',
     })
     print(resp.json())
+
+# schedule.every(10).minutes.do()
+# schedule.every().hour.do()
+# schedule.every().day.at("10:30").do()
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+# schedule.every().minute.at(":17").do(job)
+
+# schedule.every().day.at("21:13").do(send_message)
+schedule.every(5).seconds.do(send_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
