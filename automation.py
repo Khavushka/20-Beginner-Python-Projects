eda7cb69076e997b66d220e4693627bf944771d5
Khavushka <khavushka@gmail.com>
2023-03-03 21:08:14 +0100

    Adding phone number

--- automation.py
+++ automation.py
@@ -2,8 +2,14 @@
 # Importing packages 
 # pip install schedule
 # https://textbelt.com/
-# pip install pip
+# pip install requests
 
+import requests
 
-
-
+def send_message():
+    resp = requests.post('https://textbelt.com/text', {
+    'phone': '60833306',
+    'message': 'Hej, smukke',
+    'key': 'textbelt',
+    })
+    print(resp.json())
