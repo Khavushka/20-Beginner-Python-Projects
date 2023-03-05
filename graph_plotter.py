d696bd1d59f382c158dbfff7df101ecc032062b6
Khavushka <khavushka@gmail.com>
2023-03-05 17:28:02 +0100

    Graph plotter x.lim and y.lim

--- graph_plotter.py
+++ graph_plotter.py
@@ -6,12 +6,18 @@ import matplotlib.pyplot as plt
 x = [2, 4, 5, 1, 5]
 y = [2, 3, 6, 5, 8]
 
-plt.plot(x, y, label = 'line 1')
+plt.plot(x, y, color = 'blue', linestyle = 'dashed', linewidth = 2, marker = 'o', markerfacecolor = 'green', markersize = 12)
+# plt.ylim(x, y)
+plt.ylim(1, 8)
+plt.xlim(1, 8)
 
-x2 = [4, 6, 7, 8]
-y2= [3, 7, 2, 9]
 
-plt.plot(x2, y2, label ='line 2')
+# plt.plot(x, y, label = 'line 1')
+
+# x2 = [4, 6, 7, 8]
+# y2 = [3, 7, 2, 9]
+
+# plt.plot(x2, y2, label ='line 2')
 
 plt.xlabel('X axis')
 
